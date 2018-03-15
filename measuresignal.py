7799cc189d93cf1f860e8b92154ef5a363e516db
Christina Krawiec <ckrawiec@sas.upenn.edu>
2018-03-15 13:34:14 -0400

    using test fields

--- measuresignal.py
+++ measuresignal.py
@@ -1,9 +1,10 @@
+import findpairs
+import dNdMu
 import numpy as np
 import matplotlib.pyplot as plt
 import json
 import sys
 import ConfigParser
-import findpairs
 import glob
 from astropy.io import fits
 from astropy.table import Table, vstack, join
@@ -12,7 +13,7 @@ from matplotlib.pyplot import cm
 
 class DataSet():
 
-    def __init__(self, zprob_tab, data_file, id_col, output):
+    def __init__(self, zprob_tab, data_file, id_col, output, z_col=None):
         data_tab = Table.read(data_file)
         self.data = join(zprob_tab, data_tab)
         #for old output version in z-prob master branch
@@ -20,6 +21,9 @@ class DataSet():
         self.zranges = getzgroups(self.data.columns)
         self.probabilities = ['P'+str(zrange) for zrange in self.zranges]
         self.output = output
+        self.idcol = id_col
+        if z_col:
+            self.zcol = z_col
 
     def assignTypes(self, zindices, belows, aboves, check=False, magcol='MAG_AUTO_{}'):
         color = cm.rainbow(np.linspace(0,1,len(zindices)+1))
@@ -86,64 +90,64 @@ def parseconfigs(config_file):
     params['data_file'] = config.get('I/O','data_file')
     params['lens_file'] = config.get('I/O','lens_file')
     params['balrog_zp_file'] = config.get('I/O','balrog_zp_file')
-    params['balrog_data_file'] = config.get('I/O','balrog_data_file')
-    params['output'] = config.get('I/O','output')
-
-    params['test_zprob_file'] = config.get('I/O','test_zprob_file')
+    params['balrog_data_files'] = config.get('I/O','balrog_data_files')
+    params['test_zp_file'] = config.get('I/O','test_zp_file')
     params['test_data_file'] = config.get('I/O','test_data_file')
-    params['test_k_file'] = config.get('I/O','test_k_file')
+    params['output'] = config.get('I/O','output')
     
     #Table Column Names
     params['zp_id_col'] = config.get('Columns','zp_id_col')
     params['data_id_col'] = config.get('Columns','data_id_col')
     params['test_data_id_col'] = config.get('Columns','test_data_id_col')
+    params['test_z_col'] = config.get('Columns','test_z_col')
 
     #Assignment Criteria
     params['redshift_index'] = json.loads(config.get('Assignments','redshift_index'))
     params['below'] = json.loads(config.get('Assignments','below'))
     params['above'] = json.loads(config.get('Assignments','above'))
+    params['true_ranges'] = json.loads(config.get('Assignments','true_ranges'))
 
     return params
 
-def main():
+def main(args):
     #parse config file
-    params = parseconfigs('measuresignal.config')
+    params = parseconfigs(args[1])
 
     #get all z-prob files
     zp_files = glob.glob(params['zp_file'])
     sys.stderr.write('Found {} z-prob files.\n'.format(len(zp_files)))
     zp_tab = Table.read(zp_files[0])
-    sys.stderr.write('Reading files...\n')
+    sys.stderr.write('Reading files...')
     for zp_file in zp_files[1:]:
         zp_tab = vstack([zp_tab, Table.read(zp_file)])
-    sys.stderr.write('Done. Found {} objects.\n'.format(len(zp_tab)))
+    sys.stderr.write('Done.\n Found {} objects.\n'.format(len(zp_tab)))
 
     #create target and lens data objects
     objects = DataSet(zp_tab, params['data_file'], params['zp_id_col'], params['output'])
     lenses = fits.open(params['lens_file'])[1].data
     
     #assign types to objects from zprob_file based on their probabilities
-    objects.assignTypes(params['redshift_index'], params['below'], params['above'], check=False)
+    objects.assignTypes(params['redshift_index'], params['below'], params['above'], check=True)
 
-    #test files from COSMOS region
-    cosmos = DataSet(Table.read(params['test_zprob_file']), params['test_data_file'],
-                     params['test_data_id_col'], params['output']+'_test')
-    testP = Table.read(params['test_zprob_file'])
-    #testk = Table.read(params['test_k_file'])
+    #files from test region
+    test = DataSet(Table.read(params['test_zp_file']), params['test_data_file'],
+                   params['test_data_id_col'], params['output']+'_test', z_col=params['test_z_col'])
 
-    #measured number densities
+    test.assignTypes(params['redshift_index'], params['below'], params['above'], check=False)
+    
+    #measured quantities, vectors and matrices
     n_vec = []
     P_mat = []
     k_mat = []
     for objtype in objects.types:
         sys.stderr.write('Working on type {}...'.format(objtype))
         
-        #for each type - calculate correlation with lenses
-        these_params = {}
-
         #write type objects to table
         this_table = '{}_type{}.fits'.format(params['output'], objtype)
         objects.types[objtype].write(this_table)
+        
+        #for each type - create parameters for treecorr 
+        these_params = {}
         these_params['source_file'] = this_table
         these_params['lens_file'] = params['lens_file'] 
         these_params['random_source_file'] = params['lens_file'] ### 
@@ -152,20 +156,51 @@ def main():
         these_params['source_dec'] = 'DEC'
         these_params['output'] = params['output']+'_type'+str(objtype)
         
-        #call treecorr with proper config
+        #calculate correlation with lenses - call treecorr
         nn = findpairs.treecorr(these_params)
         n_vec.append(nn['npairs'])
 
         sys.stderr.write('Done.\n')
-        #get probabilities of true/false assignment
-#        P_mat.append([])
-#        k_mat.append([])
-#        for true_objtype in objects.types:
-#            P_mat[-1].append(testP['P{}P{}'.format(objtype, true_objtype)])
-#            k_mat[-1].append(testk['k{}{}'.format(objtype, true_objtype)])
+                
+        P_mat.append([])
+        k_mat.append([])
+                 
+        for true_objtype in objects.types:
+            """
+            get assigned types of detected objects
+            dn/dmu = [N_det(mu_G) - N_det(1)] / (mu_G - 1)
+            get probabilities of true/false assignment
+            Using test file, true z, assign types to test targets, get P(H|G) from test templates
+            Run dNdMu with mu_G
+           
+            k_mat[-1].append(testk['k{}{}'.format(objtype, true_objtype)])
+            """
+            z_photo = test.data[test.zcol]
+            zmin = np.min(params['true_ranges'][true_objtype])
+            zmax = np.max(params['true_ranges'][true_objtype])
+            z_true = np.where((z_photo >= zmin) & (z_photo <= zmax))
+            ids = test.data[test.idcol][z_true]
+
+            this_z_photo = test.types[objtype][test.zcol]
+            this_z_true = np.where((this_z_photo >= zmin) & (this_z_photo <= zmax))
+            these_ids = test.types[objtype][test.idcol][this_z_true]
+
+            #Assigned to this type while truly in this group
+            both = float(len(set(ids).intersection(these_ids)))
+            
+            #Save number of objects called objtype when actually true_objtype
+            P_mat[-1].append(both/float(len(ids)))
+
+
+            #itab = tab.find('tab')
+            #tabnum = tab[itab+3:itab+5]
 
     print n_vec
+    print P_mat
+
+
+
 if __name__=="__main__":
-    main()
+    main(sys.argv)
 
 
